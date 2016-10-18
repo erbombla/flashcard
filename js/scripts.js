5cb082199ba05bfa2c1eb0acb82806c180765e58
Eric Bomblatus and Viktor Grom <eric@>
2016-10-18 14:55:03 -0700

    add content-div, li, p-jquery/java and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,7 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
-
-    $("button#green").click(function() {
-      $("body").removeClass();
-      $("body").addClass("green-background");
-    });
-
-    $("button#yellow").click(function() {
-      $("body").removeClass();
-      $("body").addClass("yellow-background");
-    });
-
-    $("button#white").click(function() {
-      $("body").removeClass();
-      $("body").addClass("white-background");
-    });
+  $(".click").click(function() {
+    $("#show").toggle();
+    $("#hidden").toggle();
 
   });
 });
