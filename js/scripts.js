15a99746710df5866c7768dc9863b4de06e61783
Eric Bomblatus and Viktor Grom <viktor@>
2016-10-18 17:26:54 -0700

    added section for Variables with corresponding jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $(".click").click(function() {
     $("#show").toggle();
     $("#hidden").toggle();
-
+  });
+  $(".click1").click(function() {
+    $("#show1").toggle();
+    $("#hidden1").toggle();
   });
 });
